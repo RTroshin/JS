b7a1a9c49ceb537443fc2368c46fbea8053ee5d0
RTroshin <R.R.Troshin@outlook.com>
2020-03-03 10:15:03 +0300

    Program InputName

---
+++ InputName/InputName.js
@@ -0,0 +1,8 @@
+var name = prompt("Пожалуйста, введите своё имя");
+alert("Привет, " + name + "!");
+
+var message = "Привет, галактика!";
+alert(message);
+
+message = "Привет, мир!";
+alert(message);
