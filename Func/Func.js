0443c46bf2e5c02a67580aa0cb4935d29037badf
RTroshin <R.R.Troshin@outlook.com>
2020-03-04 14:00:05 +0300

    Program Func

---
+++ Func/Func.js
@@ -0,0 +1,24 @@
+function sayHello(name, surname)
+{
+	//alert("Hello, everybody");
+	//alert("Glad to see you");
+	//document.write("Hello, everybody <br>");
+	document.write("Hello, " + name + " " + surname + "<br>");
+	document.write("Glad to see you <br>");		
+}
+
+/*
+alert("Hello, everybody");
+alert("Glad to see you");
+*/
+
+sayHello("Mike", "Jordan");
+
+/// ...
+
+/*
+alert("Hello, everybody");
+alert("Glad to see you");
+*/
+
+sayHello("Jhon", "Gray");
